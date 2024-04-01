6f6f41fbb53a082a6af4572d64dd44acb52de615
z <z>
2024-04-01 18:57:16 +0800

    x

--- web/c3.sh
+++ web/c3.sh
@@ -1,4 +1,8 @@
 # https://www.npmjs.com/package/ts-protoc-gen
+
+# sudo snap install protobuf --classic
+# yarn add ts-protoc-gen
+# 
 rm -rf ./client/*
 # Path to this plugin, Note this must be an absolute path on Windows (see #15)
 PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
@@ -10,3 +14,5 @@ protoc \
   --plugin=protoc-gen-ts=${PROTOC_GEN_TS_PATH} \
   --ts_out=service=grpc-web:${OUT_DIR} \
   **.proto
+
+# npx babel ${OUT_DIR} --out-dir es6
---
+++ web/test1.mjs
@@ -0,0 +1,3 @@
+import {RandomizerServiceClient} from './es6/randomizer_pb_service.js'
+
+console.log('r', RandomizerServiceClient)
