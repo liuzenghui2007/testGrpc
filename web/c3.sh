0be27eca3bcca6c299320551a67cf9fc8d97f394
Your Name <you@example.com>
2024-04-01 15:03:17 +0800

    x

---
+++ web/c3.sh
@@ -0,0 +1,12 @@
+# https://www.npmjs.com/package/ts-protoc-gen
+rm -rf ./client/*
+# Path to this plugin, Note this must be an absolute path on Windows (see #15)
+PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
+
+# Directory to write generated code to (.js and .d.ts files)
+OUT_DIR="./client"
+
+protoc \
+  --plugin=protoc-gen-ts=${PROTOC_GEN_TS_PATH} \
+  --ts_out=service=grpc-web:${OUT_DIR} \
+  **.proto
