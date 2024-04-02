6087bed0be07ef18500e3757a8e02f2938b89164
zenghui <zenghui.polyseq.com>
2024-04-02 13:05:12 +0800

    x

--- web/api.js
+++ web/api.js
@@ -1,18 +1,25 @@
-// 导入必要的模块和服务客户端
+// 导入 RandomizerServiceClient
 import { RandomizerServiceClient } from "./client/es6/client/randomizer_pb_service.js";
 
 // 创建服务客户端实例
 const serviceHost = "http://localhost:5005"; // 请根据实际情况调整服务地址
 const client = new RandomizerServiceClient(serviceHost);
 
-const oneCallback = (data) => {
-  console.log(data);
-};
-const getOne = () => {
+// 定义异步函数，用于获取随机字符串
+const getOne = async () => {
+  // 创建请求对象
   const req = new proto.randomizer.Empty();
   console.log("req", req);
-  const res = client.getRandomString(req, oneCallback);
-  console.log("getOne", res);
+
+  // 调用 gRPC 服务，并等待响应
+  const response = client.getRandomString(req, {}, function (err, response) {
+    if (err) {
+      console.log(err.code);
+      console.log(err.message);
+    } else {
+      console.log(response.toObject());
+    }
+  });
 };
 
 // 导出函数
