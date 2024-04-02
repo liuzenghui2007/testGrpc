4e6064f3b975b0d777116f696946c329b9f05eec
zenghui <zenghui.liu@polyseq.com>
2024-04-03 05:51:13 +0800

    使用Promise.all并发执行5000个请求，每个都会执行。资源不足，只有1000+成功testssl

--- server2.py
+++ server2.py
@@ -1,29 +1,40 @@
 import grpc
 import randomizer_pb2
 import randomizer_pb2_grpc
-import asyncio
+import time
 import uuid
 from concurrent import futures
 
+crt = "/Users/liu/out/polyseq.com.crt"
+key = "/Users/liu/out/polyseq.com.key"
 
 class RandomizerService(randomizer_pb2_grpc.RandomizerServiceServicer):
 
-    async def GetRandomString(self, request, context):
+    def GetRandomString(self, request, context):
+        # 返回一个随机字符串
         return randomizer_pb2.RandomStringResponse(value="RandomString_" + str(uuid.uuid4()))
 
-    async def GetRandomUUIDStream(self, request, context):
+    def GetRandomUUIDStream(self, request, context):
+        # 持续返回随机UUID
         while True:
             yield randomizer_pb2.RandomUUIDResponse(uuid=str(uuid.uuid4()))
-            await asyncio.sleep(0.1)  # 使用异步sleep
+            time.sleep(100/1000)  # 暂停1秒，避免过快发送
 
 
-async def serve():
-    server = grpc.aio.server()
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     randomizer_pb2_grpc.add_RandomizerServiceServicer_to_server(
         RandomizerService(), server)
-    server.add_insecure_port('[::]:50051')
-    await server.start()
-    await server.wait_for_termination()
+   # 配置服务器以使用 TLS
+    # 配置服务器以使用 TLS
+   # Load SSL credentials
+    server_credentials = grpc.ssl_server_credentials(
+        [(open(crt, "rb").read(), open(key, "rb").read())]
+    )    
+    server.add_secure_port('127.0.0.1:50051', server_credentials)
+    server.start()
+    server.wait_for_termination()
+
 
 if __name__ == '__main__':
-    asyncio.run(serve())
+    serve()
