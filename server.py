bb747a24f17ae261e3be5144e002e65c8e58f12d
zenghui <zenghui.polyseq.com>
2024-04-02 16:32:35 +0800

    有条件停止的stream,其他stream开始运行

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import time
 import uuid
 from concurrent import futures
 
+
 class RandomizerService(randomizer_pb2_grpc.RandomizerServiceServicer):
 
     def GetRandomString(self, request, context):
@@ -15,14 +16,17 @@ class RandomizerService(randomizer_pb2_grpc.RandomizerServiceServicer):
         # 持续返回随机UUID
         while True:
             yield randomizer_pb2.RandomUUIDResponse(uuid=str(uuid.uuid4()))
-            time.sleep(1)  # 暂停1秒，避免过快发送
+            time.sleep(100/1000)  # 暂停1秒，避免过快发送
+
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    randomizer_pb2_grpc.add_RandomizerServiceServicer_to_server(RandomizerService(), server)
+    randomizer_pb2_grpc.add_RandomizerServiceServicer_to_server(
+        RandomizerService(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
+
 
 if __name__ == '__main__':
     serve()
